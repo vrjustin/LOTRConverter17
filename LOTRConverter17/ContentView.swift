34a6f3997a981f2673a29e7a3a2d8b068c7bc11a
Justin Maronde <myemailisjustin@me.com>
2024-03-18 12:15:19 -0400

    Initial Commit

---
+++ LOTRConverter17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRConverter17
+//
+//  Created by Justin Maronde on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Justin Maronde on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
