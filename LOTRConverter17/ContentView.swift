f30cb378d653d4d4be653fae5d34f8ebb7d7db17
Justin Maronde <myemailisjustin@me.com>
2024-03-18 13:16:19 -0400

    background image, modifies and placeholder textfields.

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -11,11 +11,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             //Background Image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 // Prancing Pony Image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency Exchange Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 // Currency Conversion Text
                 HStack {
@@ -24,29 +34,51 @@ struct ContentView: View {
                         //Currency
                         HStack {
                             // Currency Image
-                            
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             //Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         //Textfield
-                        
+                        Text("Textfield")
                     }
                     // Equal Sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right Conversion
                     VStack {
                         //Currency
                         HStack {
                             // Currency Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             //Currency Image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         //Textfield
-                        
+                        Text("Textfield")
                     }
                 }
-                // Info Button
+                Spacer()
                 
+                // Info Button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
+//            .border(.blue)
         }
     }
 }
