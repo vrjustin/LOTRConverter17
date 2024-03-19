e00724c835d7baf2e3851c33b8ff1899cf2f787a
Justin Maronde <myemailisjustin@me.com>
2024-03-19 08:16:07 -0400

    add in our currency model

---
+++ LOTRConverter17/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter17
+//
+//  Created by Justin Maronde on 3/19/24.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
