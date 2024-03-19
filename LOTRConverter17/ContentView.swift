5df8cf99917b4494f37194e83609b603afa131c4
Justin Maronde <myemailisjustin@me.com>
2024-03-19 13:19:13 -0400

    move onChange observers to entire zstack; use decimal pad

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -62,11 +62,8 @@ struct ContentView: View {
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
                             .focused($leftTyping)
-                            .onChange(of: leftAmount, {
-                                if leftTyping {
-                                    rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
-                                }
-                            })
+                            .keyboardType(.decimalPad)
+                            
                     }
                     // Equal Sign
                     Image(systemName: "equal")
@@ -99,11 +96,8 @@ struct ContentView: View {
                             .textFieldStyle(.roundedBorder)
                             .focused($rightTyping)
                             .multilineTextAlignment(.trailing)
-                            .onChange(of: rightAmount, {
-                                if rightTyping {
-                                    leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
-                                }
-                            })
+                            .keyboardType(.decimalPad)
+                            
                     }
                 }
                 .padding()
@@ -128,6 +122,22 @@ struct ContentView: View {
             }
 //            .border(.blue)
         }
+        .onChange(of: leftAmount, {
+            if leftTyping {
+                rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+            }
+        })
+        .onChange(of: rightAmount, {
+            if rightTyping {
+                leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+            }
+        })
+        .onChange(of: leftCurrency, {
+            leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+        })
+        .onChange(of: rightCurrency, {
+            rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+        })
         .sheet(isPresented: $showExchangeInfo, content: {
             ExchangeInfoView()
         })
